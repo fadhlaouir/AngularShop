29f9bfc6a8049386fe091f78f9d6585b3beaad7a
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-22 10:17:22 +0100

    add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularshop';
+  imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRArBwz97tp55QkQxHBPoss4b6zq4u8t5XF2w&usqp=CAU'
+  colSpan = 2
 }
