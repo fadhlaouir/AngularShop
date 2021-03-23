ca50a497f34f1c5fda8c6ec88ce40f02abe0f84b
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-23 22:15:29 +0100

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA6jBqXVRoSRPS0dli0gfW8QIQfLPUWtug",
+    authDomain: "orgshop-2e395.firebaseapp.com",
+    projectId: "orgshop-2e395",
+    storageBucket: "orgshop-2e395.appspot.com",
+    messagingSenderId: "858185064417",
+    appId: "1:858185064417:web:c1dce441dfc18fd012bca0",
+    measurementId: "G-3VK0ZCFJCP"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA6jBqXVRoSRPS0dli0gfW8QIQfLPUWtug",
+    authDomain: "orgshop-2e395.firebaseapp.com",
+    projectId: "orgshop-2e395",
+    storageBucket: "orgshop-2e395.appspot.com",
+    messagingSenderId: "858185064417",
+    appId: "1:858185064417:web:c1dce441dfc18fd012bca0",
+    measurementId: "G-3VK0ZCFJCP"
+  }
 };
 
 /*
