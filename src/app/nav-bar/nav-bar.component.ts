daa9c021c1357ab504fc2df0077b4d37004a7ca9
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-25 23:40:35 +0100

    Displaying the current user

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-
+import firebase from 'firebase'
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  user: firebase.User
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(x => console.log(x))
+    afAuth.authState.subscribe(userResponse => {
+      if (userResponse) this.user = userResponse
+    })
   }
 
   logOut() {
